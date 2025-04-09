558486c31b2d742ec7ee42054d0dc3c8eaf95ed6
Sam Cordry <samcordry@gmail.com>
2025-04-09 13:32:44 -0400

    update schema (again)

--- game/schema/consumables.sql
+++ game/schema/consumables.sql
@@ -1,6 +1,8 @@
 CREATE TABLE public.consumables (
-	"session" int4 NOT NULL,
-	"consumable" public."consumable" NOT NULL,
-	negative bool DEFAULT false NOT NULL,
-	CONSTRAINT consumables_sessions_fk FOREIGN KEY ("session") REFERENCES public.sessions("session") ON DELETE CASCADE ON UPDATE CASCADE
+    "session" int4 NOT NULL,
+    "consumable" public."consumable" NOT NULL,
+    negative bool DEFAULT false NOT NULL,
+    CONSTRAINT consumables_sessions_fk FOREIGN KEY (
+        "session"
+    ) REFERENCES public.sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
 );
--- game/schema/jokers.sql
+++ game/schema/jokers.sql
@@ -1,13 +1,18 @@
 CREATE TABLE public.jokers (
-	"session" int4 NOT NULL,
-	"joker" public."joker" NOT NULL,
-	edition public."joker_edition" DEFAULT 'base'::joker_edition NOT NULL,
-	"index" int4 NOT NULL,
-	mult int4 NULL,
-	xmult float4 NULL,
-	chips int4 NULL,
-	"rank" public."card_rank" NULL,
-	suit public."card_suit" NULL,
-	"money" int4 NULL,
-	CONSTRAINT jokers_sessions_fk FOREIGN KEY ("session") REFERENCES public.sessions("session") ON DELETE CASCADE ON UPDATE CASCADE
+    "session" int4 NOT NULL,
+    "joker" public."joker" NOT NULL,
+    edition public."joker_edition" DEFAULT 'base'::joker_edition NOT NULL,
+    "index" int4 NOT NULL,
+    mult int4 NULL,
+    xmult float4 NULL,
+    chips int4 NULL,
+    "rank" public."card_rank" NULL,
+    suit public."card_suit" NULL,
+    "money" int4 NULL,
+    sell_value int4 NOT NULL,
+    hands int4 NULL,
+    "hand_type" public."hand_type" NULL,
+    CONSTRAINT jokers_sessions_fk FOREIGN KEY (
+        "session"
+    ) REFERENCES public.sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
 );
