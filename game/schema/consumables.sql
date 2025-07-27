5938d1074d46b2b886e39ca5bab63b65994627ca
Sam Cordry <samcordry@gmail.com>
2025-07-27 13:45:51 -0400

    standardize schema files

--- game/schema/consumables.sql
+++ game/schema/consumables.sql
@@ -1,8 +1,8 @@
-CREATE TABLE public.consumables (
+CREATE TABLE consumables (
     "session" int4 NOT NULL,
-    "consumable" public."consumable" NOT NULL,
+    consumable consumable NOT NULL,
     negative bool DEFAULT false NOT NULL,
     CONSTRAINT consumables_sessions_fk FOREIGN KEY (
         "session"
-    ) REFERENCES public.sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
+    ) REFERENCES sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
 );
--- game/schema/jokers.sql
+++ game/schema/jokers.sql
@@ -1,21 +1,21 @@
-CREATE TABLE public.jokers (
+CREATE TABLE jokers (
     "session" int4 NOT NULL,
-    "joker" public."joker" NOT NULL,
-    edition public."joker_edition" DEFAULT 'base'::joker_edition NOT NULL,
+    joker joker NOT NULL,
+    edition joker_edition DEFAULT 'base'::joker_edition NOT NULL,
     "index" int4 NOT NULL,
     mult int4 NULL,
     xmult float4 NULL,
     chips int4 NULL,
-    "rank" public."card_rank" NULL,
-    suit public."card_suit" NULL,
-    "money" int4 NULL,
+    "rank" card_rank NULL,
+    suit card_suit NULL,
+    money int4 NULL,
     sell_value int4 NOT NULL,
     hands int4 NULL,
-    "hand_type" public."hand_type" NULL,
+    hand_type hand_type NULL,
     hand_size int4 NULL,
     rounds int4 NULL,
     discards int4 NULL,
     CONSTRAINT jokers_sessions_fk FOREIGN KEY (
         "session"
-    ) REFERENCES public.sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
+    ) REFERENCES sessions ("session") ON DELETE CASCADE ON UPDATE CASCADE
 );
