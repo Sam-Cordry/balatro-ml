897fc6232bb1a99a894ff17d0cb4095139be7d2b
Sam Cordry <samcordry@gmail.com>
2026-06-19 19:32:05 -0400

    balatro_core: refactor card modifications

--- balatro_core/src/cards/enhancements.rs
+++ balatro_core/src/cards/enhancements.rs
@@ -27,17 +27,17 @@ impl Enhancement {
         }
     }
 
-    pub fn on_held(&self) -> Vec<ScoreModification<'_>> {
+    pub fn on_held(&self) -> Option<ScoreModification<'_>> {
         match self {
-            Self::Steel => vec![ScoreModification::XMult(1.5)],
-            _ => vec![],
+            Self::Steel => Some(ScoreModification::XMult(1.5)),
+            _ => None,
         }
     }
 
-    pub fn on_round_end(&self) -> Vec<ScoreModification<'_>> {
+    pub fn on_round_end(&self) -> Option<ScoreModification<'_>> {
         match self {
-            Self::Gold => vec![ScoreModification::Money(3)],
-            _ => vec![],
+            Self::Gold => Some(ScoreModification::Money(3)),
+            _ => None,
         }
     }
 }
@@ -63,11 +63,6 @@ mod tests {
     }
 
     #[test]
-    fn test_wild_on_scored() {
-        assert!(Enhancement::Wild.on_scored().is_empty());
-    }
-
-    #[test]
     fn test_glass_on_scored() {
         let scoring = Enhancement::Glass.on_scored();
 
@@ -76,21 +71,11 @@ mod tests {
     }
 
     #[test]
-    fn test_steel_on_scored() {
-        assert!(Enhancement::Steel.on_scored().is_empty());
-    }
-
-    #[test]
     fn test_stone_on_scored() {
         let scoring = Enhancement::Stone.on_scored();
 
         assert_eq!(scoring.len(), 1);
         assert!(scoring.contains(&ScoreModification::Chips(50)));
-    }
-
-    #[test]
-    fn test_gold_on_scored() {
-        assert!(Enhancement::Gold.on_scored().is_empty());
     }
 
     #[test]
@@ -111,88 +96,45 @@ mod tests {
     }
 
     #[test]
-    fn test_bonus_on_held() {
-        assert!(Enhancement::Bonus.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_mult_on_held() {
-        assert!(Enhancement::Mult.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_wild_on_held() {
-        assert!(Enhancement::Wild.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_glass_on_held() {
-        assert!(Enhancement::Glass.on_held().is_empty());
+    fn test_no_effect_on_scored() {
+        assert!(Enhancement::Wild.on_scored().is_empty());
+        assert!(Enhancement::Steel.on_scored().is_empty());
+        assert!(Enhancement::Gold.on_scored().is_empty());
     }
 
     #[test]
     fn test_steel_on_held() {
         let scoring = Enhancement::Steel.on_held();
 
-        assert_eq!(scoring.len(), 1);
-        assert!(scoring.contains(&ScoreModification::XMult(1.5)));
+        assert!(scoring.is_some_and(|sm| sm == ScoreModification::XMult(1.5)));
     }
 
     #[test]
-    fn test_stone_on_held() {
-        assert!(Enhancement::Stone.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_gold_on_held() {
-        assert!(Enhancement::Gold.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_lucky_on_held() {
-        assert!(Enhancement::Lucky.on_held().is_empty());
-    }
-
-    #[test]
-    fn test_bonus_on_round_end() {
-        assert!(Enhancement::Bonus.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_mult_on_round_end() {
-        assert!(Enhancement::Mult.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_wild_on_round_end() {
-        assert!(Enhancement::Wild.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_glass_on_round_end() {
-        assert!(Enhancement::Glass.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_steel_on_round_end() {
-        assert!(Enhancement::Steel.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_stone_on_round_end() {
-        assert!(Enhancement::Stone.on_round_end().is_empty());
+    fn test_no_effect_on_held() {
+        assert!(Enhancement::Bonus.on_held().is_none());
+        assert!(Enhancement::Mult.on_held().is_none());
+        assert!(Enhancement::Wild.on_held().is_none());
+        assert!(Enhancement::Glass.on_held().is_none());
+        assert!(Enhancement::Stone.on_held().is_none());
+        assert!(Enhancement::Gold.on_held().is_none());
+        assert!(Enhancement::Lucky.on_held().is_none());
     }
 
     #[test]
     fn test_gold_on_round_end() {
         let scoring = Enhancement::Gold.on_round_end();
 
-        assert_eq!(scoring.len(), 1);
-        assert!(scoring.contains(&ScoreModification::Money(3)));
+        assert!(scoring.is_some_and(|sm| sm == ScoreModification::Money(3)));
     }
 
     #[test]
-    fn test_lucky_on_round_end() {
-        assert!(Enhancement::Lucky.on_round_end().is_empty());
+    fn test_no_effect_on_round_end() {
+        assert!(Enhancement::Bonus.on_round_end().is_none());
+        assert!(Enhancement::Mult.on_round_end().is_none());
+        assert!(Enhancement::Wild.on_round_end().is_none());
+        assert!(Enhancement::Glass.on_round_end().is_none());
+        assert!(Enhancement::Steel.on_round_end().is_none());
+        assert!(Enhancement::Stone.on_round_end().is_none());
+        assert!(Enhancement::Lucky.on_round_end().is_none());
     }
 }
--- balatro_core/src/cards/seals.rs
+++ balatro_core/src/cards/seals.rs
@@ -9,25 +9,25 @@ pub enum Seal {
 }
 
 impl Seal {
-    pub fn on_scored(&self) -> Vec<ScoreModification<'_>> {
+    pub fn on_scored(&self) -> Option<ScoreModification<'_>> {
         match self {
-            Self::Gold => vec![ScoreModification::Money(3)],
-            Self::Red => vec![], // TODO: implement when retriggers are implemented
-            _ => vec![],
+            Self::Gold => Some(ScoreModification::Money(3)),
+            Self::Red => None, // TODO: implement when retriggers are implemented
+            _ => None,
         }
     }
 
-    pub fn on_discard(&self) -> Vec<ScoreModification<'_>> {
+    pub fn on_discard(&self) -> Option<ScoreModification<'_>> {
         match self {
-            Self::Purple => vec![], // TODO: implement when tarot cards are created
-            _ => vec![],
+            Self::Purple => None, // TODO: implement when tarot cards are created
+            _ => None,
         }
     }
 
-    pub fn on_round_end(&self) -> Vec<ScoreModification<'_>> {
+    pub fn on_round_end(&self) -> Option<ScoreModification<'_>> {
         match self {
-            Self::Blue => vec![], // TODO: implement when planet cards can be returned
-            _ => vec![],
+            Self::Blue => None, // TODO: implement when planet cards can be returned
+            _ => None,
         }
     }
 }
@@ -40,53 +40,32 @@ mod tests {
     fn test_gold_seal_on_scored() {
         let scoring = Seal::Gold.on_scored();
 
-        assert_eq!(scoring.len(), 1);
-        assert!(scoring.contains(&ScoreModification::Money(3)));
+        assert!(scoring.is_some_and(|sm| sm == ScoreModification::Money(3)));
     }
 
     // TODO: create test for red seal scoring
 
     #[test]
-    fn test_blue_seal_on_scored() {
-        assert!(Seal::Blue.on_scored().is_empty());
-    }
-
-    #[test]
-    fn test_purple_seal_on_scored() {
-        assert!(Seal::Purple.on_scored().is_empty());
-    }
-
-    #[test]
-    fn test_gold_seal_on_discard() {
-        assert!(Seal::Gold.on_discard().is_empty());
-    }
-
-    #[test]
-    fn test_red_seal_on_discard() {
-        assert!(Seal::Red.on_discard().is_empty());
-    }
-
-    #[test]
-    fn test_blue_seal_on_discard() {
-        assert!(Seal::Blue.on_discard().is_empty());
+    fn test_no_effect_on_scored() {
+        assert!(Seal::Blue.on_scored().is_none());
+        assert!(Seal::Purple.on_scored().is_none());
     }
 
     // TODO: create test for purple seal discard
 
     #[test]
-    fn test_gold_seal_on_round_end() {
-        assert!(Seal::Gold.on_round_end().is_empty());
-    }
-
-    #[test]
-    fn test_red_seal_on_round_end() {
-        assert!(Seal::Red.on_round_end().is_empty());
+    fn test_no_effect_on_discard() {
+        assert!(Seal::Gold.on_discard().is_none());
+        assert!(Seal::Red.on_discard().is_none());
+        assert!(Seal::Blue.on_discard().is_none());
     }
 
     // TODO: create test for blue seal on round end
 
     #[test]
-    fn test_purple_seal_on_round_end() {
-        assert!(Seal::Purple.on_round_end().is_empty());
+    fn test_no_effect_on_round_end() {
+        assert!(Seal::Gold.on_round_end().is_none());
+        assert!(Seal::Red.on_round_end().is_none());
+        assert!(Seal::Purple.on_round_end().is_none());
     }
 }
